f9e10cc06fb91765b6446b7678bbc1f26e5c8b63
davidgil123 <131062669+davidgil123@users.noreply.github.com>
2023-06-19 00:41:57 -0500

    add carousel component

--- src/app/Components/card/card.component.ts
+++ src/app/Components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Item } from 'src/app/interfaces/item';
 import { UmanoService } from 'src/app/services/umano.service';
 
@@ -8,32 +8,12 @@ import { UmanoService } from 'src/app/services/umano.service';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
+  @Input()
   product!:Item;
   price: number= 80000;
-  products:Item []=[];
   constructor (public service:UmanoService){
-    this.products=[
-      {
-        "id":"A1",
-        "title":"Camiseta Basica",
-        "price": 80000,
-        "image": "../../../assets/Image/1.PNG",
-        "quantity": {S: 0, M: 0, L: 0},
-        "description":"Camiseta verde pino Nuestra icónica camiseta de fit regular se" +
-        "renueva en color verde pino y con el logo de TRUE estampado al frente en tono blanco." +
-        "Está confeccionada en.",
-      },
-      {
-        "id":"A2",
-        "title":"Crop Top",
-        "price": 70000,
-        "image": "../../../assets/Image/2.PNG",
-        "quantity": {S: 0, M: 0, L: 0},
-        "description":"Camiseta verde pino Nuestra icónica camiseta de fit regular se" +
-        "renueva en color verde pino y con el logo de TRUE estampado al frente en tono blanco." +
-        "Está confeccionada en.",
-      }
-    ]
+    console.log("product: "+this.product);
+
   }
   addToCart(item:Item){
     this.service.addToCar(item, {S: 1, M: 0, L: 0});
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from 'src/app/interfaces/item';
 import { UmanoService } from 'src/app/services/umano.service';
 
 @Component({
@@ -7,7 +8,59 @@ import { UmanoService } from 'src/app/services/umano.service';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent {
-constructor(public service:UmanoService){
-
-}
+  products:Item []=[];
+  constructor (public service:UmanoService){
+    this.products=[
+      {
+        "id":"A1",
+        "title":"Camiseta Basica",
+        "price": 80000,
+        "image": "../../../assets/Image/1.PNG",
+        "quantity": {S: 0, M: 0, L: 0},
+        "description":"Camiseta verde pino Nuestra icónica camiseta de fit regular se" +
+        "renueva en color verde pino y con el logo de TRUE estampado al frente en tono blanco." +
+        "Está confeccionada en.",
+      },
+      {
+        "id":"A2",
+        "title":"Crop Top",
+        "price": 70000,
+        "image": "../../../assets/Image/2.PNG",
+        "quantity": {S: 0, M: 0, L: 0},
+        "description":"Camiseta verde pino Nuestra icónica camiseta de fit regular se" +
+        "renueva en color verde pino y con el logo de TRUE estampado al frente en tono blanco." +
+        "Está confeccionada en.",
+      },
+      {
+        "id":"A3",
+        "title":"Crop Top",
+        "price": 70000,
+        "image": "../../../assets/Image/2.PNG",
+        "quantity": {S: 0, M: 0, L: 0},
+        "description":"Camiseta verde pino Nuestra icónica camiseta de fit regular se" +
+        "renueva en color verde pino y con el logo de TRUE estampado al frente en tono blanco." +
+        "Está confeccionada en.",
+      },
+      {
+        "id":"A4",
+        "title":"Crop Top",
+        "price": 70000,
+        "image": "../../../assets/Image/2.PNG",
+        "quantity": {S: 0, M: 0, L: 0},
+        "description":"Camiseta verde pino Nuestra icónica camiseta de fit regular se" +
+        "renueva en color verde pino y con el logo de TRUE estampado al frente en tono blanco." +
+        "Está confeccionada en.",
+      },
+      {
+        "id":"A5",
+        "title":"Crop Top",
+        "price": 70000,
+        "image": "../../../assets/Image/2.PNG",
+        "quantity": {S: 0, M: 0, L: 0},
+        "description":"Camiseta verde pino Nuestra icónica camiseta de fit regular se" +
+        "renueva en color verde pino y con el logo de TRUE estampado al frente en tono blanco." +
+        "Está confeccionada en.",
+      }
+    ]
+  }
 }
